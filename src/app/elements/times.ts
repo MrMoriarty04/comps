1f5e71bad976a27cd1c372864f923e0537ff0c5f
MrMoriarty04 <ramialjaber2004@gmail.com>
2025-07-22 16:47:12 +0300

    update

--- src/app/elements/times.ts
+++ src/app/elements/times.ts
@@ -1,28 +1,27 @@
-import { Directive,TemplateRef , ViewContainerRef , Input } from '@angular/core';
+import { Directive,TemplateRef,ViewContainerRef,Input } from '@angular/core';
 
 
 @Directive({
-selector: '[appTimes]'
+selector: '[Times]'
 })
 
 export class Times {
 
 constructor(
 private viewContainer: ViewContainerRef, //ref for the element we applied our directive to
-private templateRef: TemplateRef<any> // ref to elements inside container
+private templateRef:TemplateRef<any>// ref to elements inside container
 ) {}
 
   
   
 
-@Input("appTimes") set render(times:number) {
-this.viewContainer.clear();
+@Input("Times") set render(times:number) {
 
-for(let i=0;i<times;i++){
-this.viewContainer.createEmbeddedView(this.templateRef,{});
+    this.viewContainer.clear();
 
-}
-
+    for (let i = 0; i < times; i++) {
+        this.viewContainer.createEmbeddedView(this.templateRef,{});
+    }
 }
 
 }
