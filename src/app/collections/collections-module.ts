f56b381b51e42a2ecc86e973327da9a1ea183610
MrMoriarty04 <ramialjaber2004@gmail.com>
2025-07-29 12:43:58 +0300

    changes

--- src/app/collections/collections-module.ts
+++ src/app/collections/collections-module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { CollectionsRoutingModule } from './collections-routing-module';
 import { CollectionsHome } from './collections-home/collections-home';
+import { Table } from './table/table';
+import { SharedModule } from '../shared/shared-module';
 
 
 @NgModule({
   declarations: [
-    CollectionsHome
+    CollectionsHome,
+    Table
   ],
   imports: [
     CommonModule,
-    CollectionsRoutingModule
+    CollectionsRoutingModule,
+    SharedModule
   ],
   exports:[]
 })
